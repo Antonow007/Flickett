bac056f5481c00bb096b3b90a2d7fbd46ec64304
Antonow007 <115550655+Antonow007@users.noreply.github.com>
2024-02-14 13:47:07 +0200

    ...

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Security.Cryptography;
+using MySql.Data.MySqlClient;
 
 namespace Flickett
 {
@@ -21,15 +23,92 @@ namespace Flickett
             InitializeComponent();
         }
        
-        private void ForgotPassword_Click(object sender, RoutedEventArgs e)
+     
+        public void LoginButton_Click(object sender, RoutedEventArgs e) 
         {
-            MessageBox.Show("Take action for forgoten passwords");
+            string username = UsernameTextBox.Text;
+            string password = PasswordTextBox.Text;
+
+            try
+            {
+                string hashedPass = MainWindow.HashPassword(password);
+
+
+                string connstring = "server=localhost;uid=root;pwd=Antonow7;database=cinemadb;SslMode=None;";
+                using (MySqlConnection con = new MySqlConnection(connstring))
+                {
+
+
+                    con.Open();
+                    string sql = "SELECT username,passwod FROM Users WHERE username = @Username AND passwod=@Password";
+                    using (MySqlCommand cmd = new MySqlCommand(sql, con))
+                    {
+                        cmd.Parameters.AddWithValue("@Username", username);
+                        cmd.Parameters.AddWithValue("Password", hashedPass);
+                        object result = cmd.ExecuteScalar();
+
+                        if (result == null)
+                        {
+                            MessageBox.Show("User not found. Please register.");
+                        }
+                        else
+                        {
+                            string hashedPassword = HashPassword(password);
+
+                            bool passwordMatches = VerifyPassword(password, hashedPassword);
+
+
+                            if (passwordMatches)
+                            {
+                                MessageBox.Show($"Login successful: Welcome {username}!");
+                               
+
+
+                            }
+                            else
+                            {
+                                MessageBox.Show("Invalid username or password.");
+                            }
+                        }
+                    }
+                }
+
+            }
+            catch (MySqlException ex)
+            {
+
+                MessageBox.Show("Error: " + ex.Message);
+            }
         }
 
-        private void SignUp_Click(object sender, RoutedEventArgs e)
+
+        public static string HashPassword(string password)
         {
-            MessageBox.Show("Open the Registration Form");
+
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+
+            using (SHA256 sha256 = SHA256.Create())
+            {
+
+                byte[] hashedBytes = sha256.ComputeHash(passwordBytes);
+                string hashedPassword = BitConverter.ToString(hashedBytes).Replace("-", String.Empty);
+
+                return hashedPassword.Length > 50 ? hashedPassword.Substring(0, 50) : hashedPassword;
+            }
+
+
         }
+
+        static bool VerifyPassword(string password, string hashedPassword)
+        {
+            string userInputHashed = HashPassword(password);
+            return string.Equals(userInputHashed, hashedPassword, StringComparison.OrdinalIgnoreCase);
+        }
+
+
+
+       
 
     }
 }
