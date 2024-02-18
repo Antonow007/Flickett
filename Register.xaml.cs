2f0a6d49b3140b0c45246cbb99b6fcd2090ab473
Antonow007 <115550655+Antonow007@users.noreply.github.com>
2024-02-18 16:18:06 +0200

    Maked the username exist dynamic control hardly said :)

--- Register.xaml.cs
+++ Register.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Flickett.View.UserControls;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
@@ -32,9 +34,59 @@ namespace Flickett
             UsernameErrorBox.Text = "This Username has aleready taken";
         }
 
+
+        bool check = false;
+
+
         private void UsernameTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-           
+
+            if (UsenameTextBox.txtInput.Text.Length >= 3)
+            {
+
+                string username = UsenameTextBox.txtInput.Text;
+                try
+                {
+
+                    string connstring = "server=localhost;uid=root;pwd=Antonow7;database=cinemadb;SslMode=None;";
+                    using (MySqlConnection con = new MySqlConnection(connstring))
+                    {
+                        con.Open();
+                        string sql = "SELECT passwod FROM Users WHERE username = @Username";
+                        using (MySqlCommand cmd = new MySqlCommand(sql, con))
+                        {
+                            cmd.Parameters.AddWithValue("@Username", username);
+                            object result = cmd.ExecuteScalar();
+
+                            if (result == null)
+                            {
+
+
+                                UsernameErrorBox.Text = "";
+                                UsenameTextBox.txtInput.BorderBrush= System.Windows.Media.Brushes.White;
+                                check = true;
+                            }
+                            else
+                            {
+                                UsernameErrorBox.Text = "Username has already been taken";
+                                UsenameTextBox.txtInput.BorderBrush = System.Windows.Media.Brushes.Red;
+                                check = false;
+                            }
+
+                        }
+                    }
+                }
+                catch (MySqlException)
+                {
+                    MessageBox.Show("Username already exists !");
+                }
+            }
+
+
+
+
+
+
         }
     }
 }
