ab41952a3a8fe456150db0680349c0704e00f083
Antonow007 <115550655+Antonow007@users.noreply.github.com>
2024-03-18 19:31:16 +0200

    After 1 weekI succeeded то animate my login form slider panel and collected in one winfow both login and register

---
+++ Window1.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Flickett
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ThicknessAnimation animation = new ThicknessAnimation();
+            animation.From = new Thickness(0, 0, 0, 0); // Start from original position
+            animation.To = new Thickness(200, 0, 0, 0); // Adjust this value based on how much you want to slide
+            animation.Duration = TimeSpan.FromSeconds(0.4); // Duration of the animation
+
+            // Set the target property to animate (in this case, the Grid's margin)
+            myGrid.BeginAnimation(Grid.MarginProperty, animation);
+        }
+    }
+}
+
---
+++ lgnWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Flickett
+{
+    /// <summary>
+    /// Interaction logic for lgnWindow.xaml
+    /// </summary>
+    public partial class lgnWindow : Window
+    {
+        public lgnWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void MoveSliderToLeft()
+        {
+            ThicknessAnimation animation = new ThicknessAnimation();
+            animation.From = new Thickness(400, 0, 0, 0);
+            animation.To = new Thickness(0, 0, 0, 0);
+            animation.Duration = new Duration(TimeSpan.FromSeconds(0.5));
+            animation.EasingFunction = new QuadraticEase();
+
+            Slider.BeginAnimation(FrameworkElement.MarginProperty, animation);
+        }
+
+        private void MoveSliderToRight()
+        {
+            ThicknessAnimation animation = new ThicknessAnimation();
+            animation.From = new Thickness(0, 0, 0, 0);
+            animation.To = new Thickness(400, 0, 0, 0);
+            animation.Duration = new Duration(TimeSpan.FromSeconds(0.5));
+            animation.EasingFunction = new QuadraticEase();
+
+            Slider.BeginAnimation(FrameworkElement.MarginProperty, animation);
+        }
+
+        private void SignUP_Click(object sender, RoutedEventArgs e)
+        {
+            MoveSliderToLeft();
+        }
+
+        private void SignInRegister_Click(object sender, RoutedEventArgs e)
+        {
+            MoveSliderToRight();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+       
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+            WindowState = WindowState.Minimized;
+        }
+    }
+}
