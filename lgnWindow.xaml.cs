745745198caa814f83c958aee043f9d52c518830
Antonow007 <115550655+Antonow007@users.noreply.github.com>
2024-03-21 22:40:25 +0200

    Deleted the seperated old Login and Register windows and change them with the new one with all funvtionalities and code back

--- lgnWindow.xaml.cs
+++ lgnWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Security.Cryptography;
+using System.Text.RegularExpressions;
 
 
 namespace Flickett
@@ -21,14 +22,27 @@ namespace Flickett
     /// <summary>
     /// Interaction logic for lgnWindow.xaml
     /// </summary>
+    /// 
     public partial class lgnWindow : Window
     {
+
+        bool isUsernameValid = false;
+        bool isEmailValid = false;
+        bool isPhoneValid = false;
+        bool isPasswordValid = false;
+        bool isRepeatPasswordValid = false;
+
+
+        private readonly string connstring = "server=localhost;uid=root;pwd=Antonow7;database=cinemadb;SslMode=None;";
+
+
         public lgnWindow()
         {
             InitializeComponent();
         }
 
-       
+        //Login Part
+
         private void MoveSliderToLeft()
         {
             ThicknessAnimation animation = new ThicknessAnimation();
@@ -53,12 +67,12 @@ namespace Flickett
 
         private void SignUP_Click(object sender, RoutedEventArgs e)
         {
-            MoveSliderToLeft();               
+            MoveSliderToLeft();
         }
 
         private void SignInRegister_Click(object sender, RoutedEventArgs e)
         {
-            MoveSliderToRight();         
+            MoveSliderToRight();
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
@@ -69,7 +83,7 @@ namespace Flickett
 
         private void MinimizeButton_Click(object sender, RoutedEventArgs e)
         {
-           
+
             WindowState = WindowState.Minimized;
         }
 
@@ -107,66 +121,6 @@ namespace Flickett
             }
         }
 
-        private void RegisterUsernameBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (string.IsNullOrEmpty(RegisterUsernameBox.Text))
-            {
-                RegisterUsernamePlaceholder.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                RegisterUsernamePlaceholder.Visibility = Visibility.Hidden;
-            }
-        }
-
-        private void RegisterPasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
-        {
-            if (string.IsNullOrEmpty(RegisterPasswordBox.Password))
-            {
-                RegisterPasswordPlaceholder.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                RegisterPasswordPlaceholder.Visibility = Visibility.Hidden;
-            }
-        }
-
-        private void RegisterRepeatPasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
-        {
-            if (string.IsNullOrEmpty(RegisterRepeatPasswordBox.Password))
-            {
-                RegisterRepeatPasswordPlaceholder.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                RegisterRepeatPasswordPlaceholder.Visibility = Visibility.Hidden;
-            }
-        }
-
-        private void RegisterEmailBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (string.IsNullOrEmpty(RegisterEmailBox.Text))
-            {
-                RegisterEmailPlaceholder.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                RegisterEmailPlaceholder.Visibility = Visibility.Hidden;
-            }
-        }
-
-        private void RegisterPhoneBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (string.IsNullOrEmpty(RegisterPhoneBox.Text))
-            {
-                RegisterPhonePlaceholder.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                RegisterPhonePlaceholder.Visibility = Visibility.Hidden;
-            }
-        }
-
         private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
 
@@ -199,12 +153,11 @@ namespace Flickett
 
                                 if (passwordCount > 0)
                                 {
-                                    LoginPasswordBox.BorderBrush = Brushes.White;
                                     MessageBox.Show($"Login successful: Welcome {username}!");
                                 }
                                 else
                                 {
-                                    MessageBox.Show("Wrong password.");
+                                    LoginPasswordErrorBox.Text = "Wrong Password!";
                                     LoginPasswordBox.Clear();
                                     LoginPasswordBox.BorderBrush = Brushes.Red;
                                 }
@@ -224,6 +177,250 @@ namespace Flickett
 
 
 
+        }
+
+        //Register Part
+
+        private void RegisterUsernameBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(RegisterUsernameBox.Text))
+            {
+                RegisterUsernamePlaceholder.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                RegisterUsernamePlaceholder.Visibility = Visibility.Hidden;
+            }
+
+
+            string username = RegisterUsernameBox.Text;
+
+            try
+            {
+
+                using (MySqlConnection con = new MySqlConnection(connstring))
+                {
+                    con.Open();
+                    string sql = "SELECT passwod FROM Users WHERE username = @Username";
+                    using (MySqlCommand cmd = new MySqlCommand(sql, con))
+                    {
+                        cmd.Parameters.AddWithValue("@Username", username);
+                        object result = cmd.ExecuteScalar();
+
+                        if (result == null)
+                        {
+                            UsernameErrorBox.Text = "";
+                            RegisterUsernameBox.BorderBrush = System.Windows.Media.Brushes.White;
+                            isUsernameValid = true;
+                        }
+                        else
+                        {
+                            UsernameErrorBox.Text = "Username has already been taken";
+                            RegisterUsernameBox.BorderBrush = System.Windows.Media.Brushes.Red;
+                            isUsernameValid = false;
+                        }
+
+                    }
+                }
+            }
+            catch (MySqlException)
+            {
+                MessageBox.Show("Database error !");
+            }
+
+            UpdateRegisterButtonState();
+        }
+
+        private void RegisterPasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(RegisterPasswordBox.Password))
+            {
+                RegisterPasswordPlaceholder.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                RegisterPasswordPlaceholder.Visibility = Visibility.Hidden;
+            }
+            if (RegisterPasswordBox.Password.Length < 6 && RegisterPasswordBox.Password.Length != 0)
+            {
+                PasswordErrorBox.Text = "Password must be at least 6 characters";
+                RegisterPasswordBox.BorderBrush = System.Windows.Media.Brushes.Red;
+                isPasswordValid = false;
+            }
+            else
+            {
+                PasswordErrorBox.Text = "";
+                RegisterPasswordBox.BorderBrush = System.Windows.Media.Brushes.White;
+                isPasswordValid = true;
+            }
+
+            UpdateRegisterButtonState();
+        }
+
+        private void RegisterRepeatPasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(RegisterRepeatPasswordBox.Password))
+            {
+                RegisterRepeatPasswordPlaceholder.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                RegisterRepeatPasswordPlaceholder.Visibility = Visibility.Hidden;
+            }
+            if (RegisterRepeatPasswordBox.Password != RegisterPasswordBox.Password)
+            {
+                RepeatPasswordErrorBox.Text = "Passwords not match!";
+                RegisterRepeatPasswordBox.BorderBrush = System.Windows.Media.Brushes.Red;
+                isRepeatPasswordValid = false;
+            }
+            else
+            {
+                RepeatPasswordErrorBox.Text = "";
+                RegisterRepeatPasswordBox.BorderBrush = System.Windows.Media.Brushes.White;
+                isRepeatPasswordValid = true;
+            }
+
+            UpdateRegisterButtonState();
+        }
+
+        private void RegisterEmailBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(RegisterEmailBox.Text))
+            {
+                RegisterEmailPlaceholder.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                RegisterEmailPlaceholder.Visibility = Visibility.Hidden;
+            }
+
+            string email = RegisterEmailBox.Text.Trim();
+            string pattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
+
+            if (Regex.IsMatch(email, pattern))
+            {
+                EmailErrorBox.Text = "";
+                RegisterEmailBox.BorderBrush = System.Windows.Media.Brushes.White;
+                isEmailValid = true;
+            }
+            else
+            {
+                EmailErrorBox.Text = "Invalid email address!";
+                RegisterEmailBox.BorderBrush = System.Windows.Media.Brushes.Red;
+                isEmailValid = false;
+            }
+
+            UpdateRegisterButtonState();
+
+        }
+
+        private void RegisterPhoneBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(RegisterPhoneBox.Text))
+            {
+                RegisterPhonePlaceholder.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                RegisterPhonePlaceholder.Visibility = Visibility.Hidden;
+            }
+
+            string phoneNumber = RegisterPhoneBox.Text.Trim();
+            string pattern = @"^(\+359|0)8[7-9][0-9]{7}$";
+
+            if (string.IsNullOrWhiteSpace(phoneNumber))
+            {
+                PhoneErrorBox.Text = "";
+                return;
+            }
+
+            if (Regex.IsMatch(phoneNumber, pattern))
+            {
+                PhoneErrorBox.Text = "";
+                RegisterPhoneBox.BorderBrush = System.Windows.Media.Brushes.White;
+                isPhoneValid = true;
+            }
+            else
+            {
+                PhoneErrorBox.Text = "Invalid phone number!";
+                RegisterPhoneBox.BorderBrush = System.Windows.Media.Brushes.Red;
+                isPhoneValid = false;
+            }
+
+            UpdateRegisterButtonState();
+        }
+
+        private void UpdateRegisterButtonState()
+        {
+            RegisterButton.IsEnabled = isUsernameValid && isEmailValid && isPhoneValid && isPasswordValid && isRepeatPasswordValid;
+        }
+
+        private bool AllFieldsFilled()
+        {
+            return
+                   !string.IsNullOrWhiteSpace(RegisterUsernameBox.Text) &&
+                   !string.IsNullOrWhiteSpace(RegisterPasswordBox.Password) &&
+                   !string.IsNullOrWhiteSpace(RegisterRepeatPasswordBox.Password) &&
+                   !string.IsNullOrWhiteSpace(RegisterEmailBox.Text) &&
+                   !string.IsNullOrWhiteSpace(RegisterPhoneBox.Text);
+        }
+
+        private bool IsValidRegistration()
+        {
+            return isUsernameValid &&
+                isPasswordValid &&
+                isRepeatPasswordValid &&
+                isEmailValid &&
+                isPhoneValid;
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsValidRegistration() || !AllFieldsFilled())
+            {
+                MessageBox.Show("User registration failed:please check all fields !");
+                return;
+            }
+
+
+
+
+            string username = RegisterUsernameBox.Text;
+            string password = RegisterPasswordBox.Password;
+            string email = RegisterEmailBox.Text;
+            string phone = RegisterPhoneBox.Text;
+
+            try
+            {
+                string hashPassword = HashPassword(password);
+
+                using (MySqlConnection con = new MySqlConnection(connstring))
+                {
+                    con.Open();
+                    string sql = "INSERT INTO Users (username,passwod,email,phone) VALUES (@Username,@Passwod,@Email,@Phone)";
+                    using (MySqlCommand cmd = new MySqlCommand(sql, con))
+                    {
+                        cmd.Parameters.AddWithValue("@Username", username);
+                        cmd.Parameters.AddWithValue("@Passwod", hashPassword);
+                        cmd.Parameters.AddWithValue("@Email", email);
+                        cmd.Parameters.AddWithValue("@Phone", phone);
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            MoveSliderToRight();
+                        }
+                        else
+                        {
+                            MessageBox.Show("User registration failed.");
+                        }
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
         }
 
         public static string HashPassword(string password)
